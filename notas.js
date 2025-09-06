73319a496d6507e120f12a51b1aa228de14e2c2d
aneschar28 <aechavarriaa1128@gmail.com>
2025-09-06 13:05:03 -0500

    Update notas.js

--- notas.js
+++ notas.js
@@ -1,19 +1,17 @@
-
 function standardizeName(name) {
   return name
-  .trim()
-  .toLowerCase()
-  .split(/\s+/)
-  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-  .join(' ');
+    .trim()
+    .toLowerCase()
+    .split(/\s+/)
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 }
 
 function limpiar(name) {
   return name
-  .trim()
-  .split(/\s+/)
-  .join(' ');
-    
+    .trim()
+    .split(/\s+/)
+    .join(' ');
 }
 
 const estadosUSA = [
@@ -67,59 +65,70 @@ const estadosUSA = [
   ["Virginia Occidental", "Wv"],
   ["Wisconsin", "Wi"],
   ["Wyoming", "Wy"],
-  ["Puerto Rico", "Pr"]
+  ["Puerto Rico", "Pr"],
+  ["Made-by", "AndresChavarria&zendeskColombiateam"]
 ];
 
+let memory;
 
-function encEstado(estado){
-
-    for(array of estadosUSA){
-
-        if (array[1]===estado){
-          return array[0];
-        }
-
-        if (array[0]===estado){
-          return array[1];
-        }
-        
-
+function encEstado(estado) {
+  for (array of estadosUSA) {
+    if (array[1] === estado) {
+      return array[0];
     }
-
-    return "no data"
-
+    if (array[0] === estado) {
+      return array[1];
+    }
+  }
+  return "no data";
 }
-
 
 document.getElementById("logForm").addEventListener("input", function (e) {
   e.preventDefault();
 
   const formData = new FormData(this);
-  const log = `CONTACT VIA: ${limpiar(formData.get("via") + " " +"#" + " " + formData.get("number"))} //// NAME: ${standardizeName(formData.get("name"))} //// ORDER: ${limpiar(formData.get("order"))} //// INQUIRY: ${limpiar(formData.get("inquiry"))} //// ACTION TAKEN: ${limpiar(formData.get("actionTaken"))} //// CEX: ${limpiar(formData.get("cex"))} //// WHY: ${limpiar(formData.get("reason")+" "+formData.get("other"))}`;
+  const log = `CONTACT VIA: ${limpiar(formData.get("via") + " " + "#" + " " + formData.get("number"))} //// NAME: ${standardizeName(formData.get("name"))} //// ORDER: ${limpiar(formData.get("order"))} //// INQUIRY: ${limpiar(formData.get("inquiry"))} //// ACTION TAKEN: ${limpiar(formData.get("actionTaken"))} //// CEX: ${limpiar(formData.get("cex"))} //// WHY: ${limpiar(formData.get("reason") + " " + formData.get("other"))}`;
 
+  document.getElementById("number").childNodes[0].nodeValue = formData.get("via") + " number";
   document.getElementById("output").textContent = log;
+  document.getElementById("botones").innerHTML = `<button id = copy>copy</button>`;
+
+  document.getElementById("copy").addEventListener("click", function () {
+    const texto = document.getElementById("output").textContent;
+    navigator.clipboard.writeText(texto).catch(err => console.error("Error al copiar: ", err));
+    document.getElementById("output").className = "output"
+    setTimeout(() => document.getElementById("output").className = "", 500);
+  });
 });
 
 document.getElementById("clean").addEventListener("click", function (e) {
   e.preventDefault();
 
   const logForm = document.getElementById("logForm");
-  logForm.reset();
-  document.getElementById("output").textContent = "";
-  
-});
+  console.log(logForm);
 
-document.getElementById("copy").addEventListener("click", function () {
-  
+  Array.from(logForm.elements).forEach(el => {
+    if (el.name !== "via") {
+      el.value = "";
+    }
+  });
+
+  memory = document.getElementById("output").textContent;
+  document.getElementById("output").textContent = "";
+  document.getElementById("botones").innerHTML = `<button id = copy>copy</button><button id = last>last note</button>`;
+
+  document.getElementById("last").addEventListener("click", function () {
+    document.getElementById("output").textContent = memory;
+  });
+
+  document.getElementById("copy").addEventListener("click", function () {
     const texto = document.getElementById("output").textContent;
     navigator.clipboard.writeText(texto).catch(err => console.error("Error al copiar: ", err));
-  
+    document.getElementById("output").className = "output"
+    setTimeout(() => document.getElementById("output").className = "", 500);
+  });
 });
 
 document.getElementById("serch").addEventListener("click", function () {
-  
-    document.getElementById("respuesta").innerText = encEstado(standardizeName(document.getElementById("abrev").value));
-
+  document.getElementById("respuesta").innerText = encEstado(standardizeName(document.getElementById("abrev").value));
 });
-
-
